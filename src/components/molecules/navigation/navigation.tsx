2749a11a7c605cfca432e8468c65c5a3357c0fd3
Robin Grahl <RobinGrahl@gmail.com>
2025-04-28 22:23:30 +0200

    current broken style

--- src/components/molecules/navigation/navigation.tsx
+++ src/components/molecules/navigation/navigation.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import Link from "next/link";
-import { ArrowLeft } from "lucide-react";
+import { ArrowLeft, Mail, Plus, ScanBarcode } from "lucide-react";
 import { useState } from "react";
 import "./navigation.css";
+import { Button } from "@/components/ui/button";
 
 interface NavigationProps {
   collapsed: boolean;
@@ -22,11 +23,14 @@ export default function Navigation({ collapsed }: NavigationProps) {
         />
       </div>
       <ul className="block">
+        <Button className="p-0 m-0 h-16 w-16">
+          <Mail size={100} />
+        </Button>
         <li>
-          <Link href="#">Home</Link>
-        </li>
-        <li>
-          <Link href="#">Dashboard</Link>
+          <Link href="#">
+            <ScanBarcode />
+            <span>Discounts</span>
+          </Link>
         </li>
       </ul>
     </nav>
