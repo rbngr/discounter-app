19c5bdea2f31f9c372955ed439d7a0efff364e86
Robin Grahl <RobinGrahl@gmail.com>
2025-04-28 21:40:47 +0200

    doing navigation step by step

--- src/components/molecules/navigation/navigation.tsx
+++ src/components/molecules/navigation/navigation.tsx
@@ -1,13 +1,33 @@
+"use client";
+
 import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
+import { useState } from "react";
 
 interface NavigationProps {
   collapsed: boolean;
 }
 
 export default function Navigation({ collapsed }: NavigationProps) {
+  const [isCollapsed, setIsCollapsed] = useState(collapsed);
+
   return (
-    <nav className={"h-full p-4 bg-slate-100 " + (collapsed ? "w-16" : "w-64")}>
-      <div></div>
+    <nav
+      className={
+        "h-full p-4 bg-slate-100 transition-all " +
+        (isCollapsed ? "w-16" : "w-64")
+      }
+    >
+      <div className={"flex justify-end items-center"}>
+        <ArrowLeft
+          size={24}
+          className={
+            "text-neutral-500 cursor-pointer " +
+            (isCollapsed ? "rotate-180" : "rotate-0")
+          }
+          onClick={() => setIsCollapsed(!isCollapsed)}
+        />
+      </div>
       <ul className="block">
         <li>
           <Link href="#">Home</Link>
